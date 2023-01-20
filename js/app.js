2169c5d84655efc4e360e5770f063488d864e47d
Ilya <61752920+ilyadudiak@users.noreply.github.com>
2023-01-20 20:19:58 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$('.main').slick({
+    infinite: true,
+    slidesToShow: 1,
+    draggable: false,
+    rows: 1,
+    slidesPerRow: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+});
